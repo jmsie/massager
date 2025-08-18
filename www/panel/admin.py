ce5fcb6634583e43b4d4b087a7059c1a7185e671
Jeff Lu <jeff@gogoout.com>
2025-08-18 18:44:03 +0800

    update admin

--- www/panel/admin.py
+++ www/panel/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Therapist, Specialization, Store
+from .models import Therapist, Specialization, Store, MassagePlan, ServiceSurvey, Reservation, MassageInvitation
 
 # Register your models here.
 admin.site.register(Therapist)
 admin.site.register(Specialization)
 admin.site.register(Store)
+admin.site.register(MassagePlan)
+admin.site.register(ServiceSurvey)
+admin.site.register(Reservation)
+admin.site.register(MassageInvitation)
